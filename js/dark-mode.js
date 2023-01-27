540c5863953fc943a7b42da04ba3c6c83bfc3cfb
sbalint2012 <sipos.balint07@gmail.com>
2023-01-27 14:23:07 +0100

    dark mode add memory

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,4 +1,5 @@
 function myFunction() {
+
     let banner = document.getElementById("banner-image");
     var element = document.body;
     element.classList.toggle("dark-mode");
@@ -10,3 +11,14 @@ function myFunction() {
     }
   
 }
+
+window.onload=function() {
+    if(localStorage.darkMode=="true") {
+        myFunction();
+        document.getElementById("myCheck").checked=true;
+    }
+};
+
+document.getElementById("myCheck").addEventListener("change", () => {
+    localStorage.darkMode=(localStorage.darkMode=="true")?"false":"true";
+});
